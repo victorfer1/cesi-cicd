79cac563c847543e00ebb6f9f0cebe44c835f3f0
victorfer1 <fer.victor220@gmail.com>
2023-08-21 09:29:34 +0200

    Test deploy

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1>About Page</h1>
+        <h1>About Pages</h1>
         <p className={styles.description}>
           <Link href="/">&larr; Go Back</Link>
         </p>
